04469b11dc5a49241d6593ed19f97065edfc5f2c
Murat Koptur <mkoptur3@gmail.com>
2021-05-01 23:25:12 +0300

    plot2d subroutine added

--- Makefile
+++ Makefile
@@ -2,13 +2,16 @@ COMPILER = gfortran
 FLAGS = -O2
 EXEC = bin/main
 
-OBJS = Constants.o
+OBJS = Constants.o Plots.o
 
 all: src/main.f90 $(OBJS)
 	$(COMPILER) $(FLAGS) -o $(EXEC) src/main.f90 $(OBJS)
 	
 Constants.o: src/Constants.f90
 	$(COMPILER) $(FLAGS) -c src/Constants.f90
+	
+Plots.o: src/Plots.f90
+	$(COMPILER) $(FLAGS) -c src/Plots.f90
 	
 plot: src/main_plot.f90 $(OBJS)
 	$(COMPILER) $(FLAGS) -o $(EXEC) src/main_plot.f90 $(OBJS)
--- src/main_plot.f90
+++ src/main_plot.f90
@@ -7,20 +7,12 @@
 ! ============================================================================
 
 program main_plot
-    use Constants
+    use Plots
     implicit none
 
     integer, parameter    :: M = 50
-    integer               :: x, y, i, j
+    integer               :: x, y, i
     real, dimension(M, 2) :: matrix
-
-    print '(a)', 'set datafile separator ","'
-    print '(a)', 'set title "Plot Example"'
-    print '(a)', 'set nokey'
-    print '(a)', 'set grid'
-    print '(a)', 'set xlabel "x"'
-    print '(a)', 'set ylabel "y"'
-    print '(a)', 'plot "-" using 1:2 with lines'
 
     matrix(1, 1) = 0.0
     matrix(1, 2) = 0.0
@@ -30,6 +22,6 @@ program main_plot
         matrix(i, 2) = matrix(i, 1)**2
     end do
 
-    print '((F8.5, ",", F8.5))', ((matrix(i,j), j = 1, 2), i = 1, M)
+    call plot2d(matrix)
 
 end program
